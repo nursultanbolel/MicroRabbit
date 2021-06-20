fcd0f421349f07d8e1ef2ce257f55377cd5cc15e
Nur Sultan Bolel <nursultanbolel@gmail.com>
2021-06-20 15:02:19 +0300

    Implemented Interfaces and classes for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    //Any request was sent ot processed using we are expecting a boolean back true or false,
+    public abstract class Message: IRequest<bool>
+    {
+        public string MessageType { get; protected set; }
+
+        protected Message()
+        {
+            MessageType = GetType().Name;
+        }
+    }
+}
